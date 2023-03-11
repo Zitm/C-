8a0b19abef9f1ffe6c8978fc2afa0c595d8b62c6
Zitm <117082442+Zitm@users.noreply.github.com>
2023-03-11 20:29:56 +0400

    Update dz7 z1.cpp

--- dz7 z1.cpp
+++ dz7 z1.cpp
@@ -2,13 +2,31 @@
 
 main()
 {
-	int sum,i;
-	sum=0;
-	i=5;
-	while(i<16)
+	int i=1;
+	while(i<10)
 	{
-		sum=sum+i;
-		i=++i;
+		printf("hello\n");
+		i++;
 	}
-	printf("%i",sum);
+	
+	int b=0;
+	int c=0;
+	while(b<16)
+	{
+		printf("%i\n",c++);
+		b++;
+	}
+	
+	int d=0;
+	int e=0;
+	while(d<16)
+	{
+		e++;
+		if(e%2!=0)
+		{
+			printf("%i\n",e);
+		}
+			
+		d++;
+	}
 }
