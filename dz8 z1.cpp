ba118b953e815f8acb5362846517041609f98ea4
Zitm <117082442+Zitm@users.noreply.github.com>
2023-03-11 20:39:45 +0400

    Update dz8 z1.cpp

--- dz8 z1.cpp
+++ dz8 z1.cpp
@@ -2,45 +2,12 @@
 
 main()
 {
-	int i;
-	while(i!=7)
+	
+	int i=1;
+	int b=5;
+	while(i<11)
 	{
-		scanf("%i",&i);
-		
-		if(i<10)
-		{
-			printf("menshe 10\n");
-		}
-		if(i%2==0)
-		{
-			printf("delitsya na 2\n");
-		}
-		else
-		{
-			printf("ne delitsya na 2\n");
-		}
-		if(i%3==0)
-		{
-			printf("delitsya na 3\n");
-		}
-		else
-		{
-			printf("ne delitsya na 3\n");
-		}
-		if(i<7)
-		{
-			printf("bolshe\n");
-		}
-		else
-		{
-			if(i>7)
-			{
-				printf("menshe\n");
-			}
-			else
-			{
-				printf("pravilno\n");
-			}
-		}
+		printf("%i*%i=%i\n",b,i,b*i);
+		i++;
 	}
 }
